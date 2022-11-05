b979542dc10fc4dc58f6e388179bcf473875be48
EddyGiusepe <eddychirinos.unac@gmail.com>
2022-11-05 15:07:28 -0300

    Modelo training

--- app.py
+++ app.py
@@ -2,11 +2,17 @@
 PhD.:Eddy Giusepe Chirinos Isidro
 '''
 
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS # Para poder fazer conexão desde qualquer parte do mundo
+import torch
+import numpy as np
+from PIL import Image
+
 
 app = Flask(__name__)
 CORS(app) # para a conexão desde qualquer lugar do mundo
+
+model = torch.jit.load('model.zip')
 
 
 @app.route('/')
@@ -15,10 +21,28 @@ def hello_world():
 
 
 # Podemos fazer outra rota, assim:
-@app.route("/predict")
+@app.route("/predict", methods=["POST"])
 def predict():
-    return 'predições'
+    
+    # load image
+    img = Image.open(request.files['file'].stream).convert(
+        'RGB').resize((224, 224))
+    img = np.array(img)
+    img = torch.FloatTensor(img.transpose((2, 0, 1)) / 255)
 
+    # get predictions
+    preds = model(img.unsqueeze(0)).squeeze()
+    probas = torch.softmax(preds, axis=0)
+    ix = torch.argmax(probas, axis=0)
+
+    return {
+        'label': model.labels[ix],
+        'score': probas[ix].item()
+    }
+
+
+# Podemos fazer na terminal, também, assim:
+# ...$ curl http://127.0.0.1:5000/ 
 
     
 
